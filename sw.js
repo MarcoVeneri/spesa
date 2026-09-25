f395d02d78fbfda8cac277354bd15084984bfe02
MarcoVeneri <30596631+MarcoVeneri@users.noreply.github.com>
2026-09-25 15:07:21 -0700

    Refresh shopping cache v13

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='spesa-v12';
-const ASSETS=['./','./index.html','./styles.css?v=12','./app.js?v=12','./manifest.webmanifest','./icon.svg'];
+const CACHE='spesa-v13';
+const ASSETS=['./','./index.html','./styles.css?v=13','./app.js?v=13','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
   self.skipWaiting();
