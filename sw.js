5b6a1d7975c8940a8f203a76e53b7f8273b55924
MarcoVeneri <30596631+MarcoVeneri@users.noreply.github.com>
2026-09-25 14:49:38 -0700

    Refresh shopping app cache after text size update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='spesa-v3';
+const CACHE='spesa-v4';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
