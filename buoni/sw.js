a13e60ec514f338c49abc71367cde28d6b644266
MarcoVeneri <30596631+MarcoVeneri@users.noreply.github.com>
2026-09-26 05:59:31 -0700

    Refresh Buoni Spesa cache after voucher fixes

--- buoni/sw.js
+++ buoni/sw.js
@@ -1,4 +1,4 @@
-const CACHE='buoni-spesa-v6';
+const CACHE='buoni-spesa-v7';
 const ASSETS=['./','./index.html','./manifest.json','./apple-touch-icon.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
