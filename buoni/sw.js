e79ab47168792b9c1bb176962a1cbe0bc9a8ef5d
MarcoVeneri <30596631+MarcoVeneri@users.noreply.github.com>
2026-09-26 02:47:09 -0700

    Add Buoni Spesa PWA

---
+++ buoni/sw.js
@@ -0,0 +1,10 @@
+const CACHE='buoni-spesa-v1';
+const ASSETS=['./','./index.html','./manifest.json','./icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{
+    const clone=resp.clone(); caches.open(CACHE).then(c=>c.put(e.request,clone)); return resp;
+  }).catch(()=>caches.match('./index.html'))));
+});
